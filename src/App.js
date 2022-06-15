0743a49cd083797a624f0ec4e040e41b17536a6f
Xavimat <xaviermatoses@gmail.com>
2022-06-15 21:01:36 +0200

    Reoutes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import { GlobalProvider } from "./context/GlobalState";
+import Home from "./components/Home/Home";
+import NewsList from "./components/NewsList/NewsList";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
     <>
-
+      <BrowserRouter>
         <Header />
         <GlobalProvider>
-
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/list" element={<NewsList />} />
+            <Route path="/form" element={<Form />} />
+            <Form />
+          </Routes>
         </GlobalProvider>
         <Footer />
-
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <div>Header</div>
+    <div>Header
+    <Link to="/">Home</Link>
+    <Link to="/list">News List</Link>
+    <Link to="/form">Form</Link>
+    </div>
   )
 }
 
